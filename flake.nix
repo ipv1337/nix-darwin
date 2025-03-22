85506bc247968fbe5374c820a17710dfb5683ced
James H. Nguyen <james.nguyen@flyr.com>
2025-03-21 23:56:38 -0700

    Update nix

--- flake.nix
+++ flake.nix
@@ -5,10 +5,11 @@
   # This is the standard format for flake.nix. `inputs` are the dependencies of the flake,
   # Each item in `inputs` will be passed as a parameter to the `outputs` function after being pulled and built.
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-locked.url = "github:NixOS/nixpkgs/1042fd8b148a9105f3c0aca3a6177fd1d9360ba5";
-    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-24.11";
+    nixpkgs-locked.url =
+      "github:NixOS/nixpkgs/1042fd8b148a9105f3c0aca3a6177fd1d9360ba5";
+    nix-darwin.url = "github:LnL7/nix-darwin/nix-darwin-25.05";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
@@ -19,44 +20,45 @@
   # parameters in `outputs` are defined in `inputs` and can be referenced by their names.
   # However, `self` is an exception, this special parameter points to the `outputs` itself (self-reference)
   # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
-  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, nix-darwin, home-manager, ... }:
-  let
-    configuration = { pkgs, ... }: {
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+  outputs =
+    inputs@{ self, nixpkgs, nixpkgs-unstable, nix-darwin, home-manager, ... }:
+    let
+      configuration = { pkgs, ... }: {
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
+      };
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#James-MacBook-Pro
+      darwinConfigurations."James-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          configuration
+          ./modules/nix-core.nix
+          ./modules/system.nix
+          ./modules/apps.nix
+          ./modules/host-users.nix
+          home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.backupFileExtension = "backup";
+            home-manager.verbose = true;
+            home-manager.users.james = import ./home/home.nix;
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+            home-manager.extraSpecialArgs = {
+              inherit inputs;
+              #dotfiles = dotfiles;
+              # hack around nix-home-manager causing infinite recursion
+              isLinux = false;
+            };
+          }
+        ];
+        specialArgs = { inherit inputs; };
+      };
+      # Expose the package set, including overlays, for convenience.
+      darwinPackages = self.darwinConfigurations."James-MacBook-Pro".pkgs;
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#James-MacBook-Pro
-    darwinConfigurations."James-MacBook-Pro" = nix-darwin.lib.darwinSystem {
-      system = "aarch64-darwin";
-      modules = [
-        configuration
-        ./modules/nix-core.nix
-        ./modules/system.nix
-        ./modules/apps.nix
-        ./modules/host-users.nix
-        home-manager.darwinModules.home-manager {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-          home-manager.backupFileExtension = "backup";
-          home-manager.verbose = true;
-          home-manager.users.james = import ./home/home.nix;
-          # Optionally, use home-manager.extraSpecialArgs to pass
-          # arguments to home.nix
-          home-manager.extraSpecialArgs = {
-            inherit inputs;
-            #dotfiles = dotfiles;
-            # hack around nix-home-manager causing infinite recursion
-            isLinux = false;
-          };
-        }
-      ];
-      specialArgs = { inherit inputs; };
-    };
-    # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."James-MacBook-Pro".pkgs;
-  };
 
 }
